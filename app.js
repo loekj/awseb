7024d679648e8937361c699d885cec535e49884b
loekj <ljanssen@stanford.edu>
2015-11-08 14:26:28 -0800

    ASD

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ var logger = require('morgan');
 // var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+/*
+* MySQL driver
+*/
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+});
+
 var app = express();
 
 app.use(logger('dev'));
@@ -19,7 +30,7 @@ app.get('/fulfillment', function(req, res, next) {
   var ful_query = req.body;
   var tests_arr = ful_query.data.tests;
   for(i=0; i < tests_arr.length; i++) {
-    console.log(i);
+    res.json(connection);
     if (tests_arr[i].activeVariation.toLowerCase() == 'null') {
       // person is not in test yet. Activate new test
       break;
