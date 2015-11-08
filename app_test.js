2bcaed4f662417d72aeeac82860a5fca01adf397
loekj <ljanssen@stanford.edu>
2015-11-08 13:58:47 -0800

    ADASDASD

--- app_test.js
+++ app_test.js
@@ -14,12 +14,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 * API dir
 */
 app.get('/fulfillment', function(req, res, next) {
-	var ful_query = req['query'];
-	ful_query.tests.activeVariation
-	res.json("asd");
+	var ful_query = req.body;
+	var tests_arr = ful_query.data.tests;
+	for(i=0; i < tests_arr.length; i++) {
+		if (tests_arr[i].activeVariation.toLowerCase() == 'null') {
+			// person is not in test yet. Activate new test
+			break;
+		} else {
+			// person is already in test, feed already active variation in response!
+			break;
+		}
+	}
+	next();
 });
 
 server.listen(3000, 'localhost');
 server.on('listening', function() {
     console.log('Express server started on port %s at %s', server.address().port, server.address().address);
 });
+
+
+//curl -v -H "Accept: application/json" -H "Content-type: application/json" -X GET -d ' {"data":{"userUuid":"attl3nZ3k41fH6wtrA3J3SQJo","tests":{"experimentUuid": "test123", "activeVariation": "null"}}}'  http://127.0.0.1:3000/fulfillment
