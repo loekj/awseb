50227673febb163a3eea68de1d4d65149d2dcaf2
loekj <ljanssen@stanford.edu>
2016-02-03 19:41:41 -0800

    ASD

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
 // app.use(function(req, res, next) {
